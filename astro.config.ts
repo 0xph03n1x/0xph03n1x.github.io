a2cfe011fcaf9ef12bd7aae512f1cfa96eed81a7
0xph03n1x <lazarovmihail97@gmail.com>
2025-02-15 23:51:04 +0200

    init repo

---
+++ astro.config.ts
@@ -0,0 +1,53 @@
+import { defineConfig } from 'astro/config';
+
+import expressiveCode from 'astro-expressive-code';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import spectre from './package/src';
+
+import node from '@astrojs/node';
+import { spectreDark } from './src/ec-theme';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://0xph03n1x.github.io',
+  output: 'static',
+  integrations: [
+    expressiveCode({
+      themes: [spectreDark],
+    }),
+    mdx(),
+    sitemap(),
+    spectre({
+      name: 'Mihail Lazarov',
+      openGraph: {
+        home: {
+          title: 'Mihail Lazarov',
+          description: 'Mihil Lazarov\'s personal blog.'
+        },
+        blog: {
+          title: 'Blog',
+          description: 'News and guides.'
+        },
+        projects: {
+          title: 'Projects',
+          description: 'Personal projects.'
+        }
+      },
+      // giscus: {
+      //   repository: 'louisescher/spectre',
+      //   repositoryId: 'R_kgDONjm3ig',
+      //   category: 'General',
+      //   categoryId: 'DIC_kwDONjm3is4ClmBF',
+      //   mapping: 'pathname',
+      //   strict: true,
+      //   reactionsEnabled: true,
+      //   emitMetadata: false,
+      //   lang: 'en',
+      // }
+    })
+  ],
+  adapter: node({
+    mode: 'standalone'
+  })
+});
